43e3e850ab292bd56b16e41f26b97338af946b42
tzeeeeee@gmail.com <tzeeeeee@gmail.com>
2025-02-26 17:08:26 +0900

    Feat : 드롭다운 props 에 넓이,높이 추가

--- src/pages/calendar/CalendarModal.tsx
+++ src/pages/calendar/CalendarModal.tsx
@@ -1,7 +1,8 @@
 // MemoModal.tsx
 import React from 'react';
 import styled from 'styled-components';
-import DropdownSample from '../../widgets/dropdown/dropdownSample';
+import Dropdown from '../../widgets/dropdown/Dropdown';
+import Button from '../../widgets/button/Button';
 
 // Styled Components
 const ModalOverlay = styled.div`
@@ -46,52 +47,21 @@ const MemoHeader = styled.div`
 `;
 
 const MemoTextarea = styled.textarea`
+  display: flex;
   width: 360px;
-  min-height: 90px;
-  padding: 8px;
-  border: 1px solid #ddd;
-  border-radius: 4px;
-  font-family: inherit;
-  margin-bottom: 16px;
-  resize: vertical;
+  height: 40px;
+  flex-direction: column;
+  justify-content: center;
+  align-items: flex-start;
+  gap: 10px;
+  align-self: stretch;
+  border-radius: 8px;
 `;
 
 const ModalActions = styled.div`
   display: flex;
   justify-content: flex-end;
   gap: 8px;
-`;
-
-const Button = styled.button`
-  display: flex;
-  width: 178px;
-  height: 40px;
-  padding: 0px 40px;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  gap: 10px;
-  align-self: stretch;
-  border: none;
-  border-radius: 8px;
-  cursor: pointer;
-  font-weight: 400;
-
-  &.cancel {
-    background-color: #f5f5f5;
-    color: #333;
-    &:hover {
-      background-color: #e0e0e0;
-    }
-  }
-
-  &.save {
-    background-color: #2ac1bc;
-    color: white;
-    &:hover {
-      background-color: #2ac1bc;
-    }
-  }
 `;
 
 interface MemoModalProps {
@@ -124,9 +94,19 @@ const CalendarModal: React.FC<MemoModalProps> = ({
           placeholder="업무 제목을 입력하세요"
         />
         <MemoHeader>일정 유형</MemoHeader>
-        <label>
-          <DropdownSample />
-        </label>
+
+        <Dropdown
+          title="일정 유형을 선택 해주세요"
+          options={[
+            { label: '회의', value: '1' },
+            { label: '출장', value: '2' },
+            { label: '회식', value: '3' },
+            { label: '휴가', value: '4' },
+            { label: '회의', value: '5' },
+          ]}
+          width="360px"
+        />
+
         <MemoHeader>내용</MemoHeader>
         <MemoTextarea
           value={memoText}
@@ -142,12 +122,9 @@ const CalendarModal: React.FC<MemoModalProps> = ({
           <input type="date" />
         </label>
         <ModalActions>
-          <Button className="cancel" onClick={onClose}>
-            취소
-          </Button>
-          <Button className="save" onClick={onSave}>
-            저장
-          </Button>
+          <Button onClick={onClose}>삭제</Button>
+
+          <Button onClick={onSave}>저장</Button>
         </ModalActions>
       </ModalContent>
     </ModalOverlay>
