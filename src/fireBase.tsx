ea5e4191fe315a83c7fd37d63971a277f3b0cb14
임하림 <gkfla668@naver.com>
2025-02-27 23:50:35 +0900

    Feat: 로그인 페이지 (#14)
    * feat: firebase config type 선언
    
    * package: 로그인을 위한 react-hook-form 설치
    
    * Feat : 로그인 페이지
    - 파이어베이스 로그인 기능 구현
    - 유효성 처리
    - 퍼블리싱
    
    * Style : 주석 추가

--- src/fireBase.tsx
+++ src/fireBase.tsx
@@ -1,5 +1,20 @@
 import { initializeApp } from 'firebase/app';
-import { getAnalytics } from 'firebase/analytics';
+
+interface ImportMetaEnv {
+  VITE_FIREBASE_APP_KEY: string;
+  VITE_FIREBASE_AUTH_DOMAIN: string;
+  VITE_FIREBASE_PROJECT_ID: string;
+  VITE_FIREBASE_STORAGE_BUCKET: string;
+  VITE_FIREBASE_MESSAGING_SENDER_ID: string;
+  VITE_FIREBASE_APP_ID: string;
+  VITE_FIREBASE_MEASUREMENTID: string;
+}
+
+declare global {
+  interface ImportMeta {
+    readonly env: ImportMetaEnv;
+  }
+}
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_APP_KEY,
@@ -13,4 +28,5 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+
+export default app;
---
+++ src/pages/login/index.tsx
@@ -0,0 +1,107 @@
+import { useForm } from 'react-hook-form';
+
+import { FirebaseError } from 'firebase/app';
+import app from '../../fireBase';
+import { getAuth, signInWithEmailAndPassword } from 'firebase/auth';
+
+import * as S from './style';
+import Button from '../../widgets/button/Button';
+
+interface LoginType {
+  email: string;
+  password: string;
+}
+
+const Login = () => {
+  const auth = getAuth(app);
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    setError,
+  } = useForm<LoginType>();
+  const handleLogin = async (data: LoginType) => {
+    try {
+      const userCredential = await signInWithEmailAndPassword(
+        auth,
+        data.email,
+        data.password
+      );
+      console.log(userCredential.user); // 추후 저장 필요
+      alert('로그인 성공!'); // 임시
+    } catch (error) {
+      const firebaseError = error as FirebaseError;
+
+      switch (firebaseError.code) {
+        case 'auth/invalid-credential':
+          setError('password', {
+            message:
+              '이메일 또는 비밀번호가 잘못되었습니다. 다시 입력해 주세요.',
+          });
+          break;
+        case 'auth/too-many-requests':
+          setError('email', {
+            message: '로그인 시도가 너무 많습니다. 잠시 후 다시 시도해 주세요.',
+          });
+          break;
+        case 'auth/network-request-failed':
+          setError('email', {
+            message:
+              '네트워크 오류가 발생했습니다. 인터넷 연결을 확인해 주세요.',
+          });
+          break;
+        default:
+          setError('email', {
+            message: '로그인 중 문제가 발생했습니다. 다시 시도해 주세요.',
+          });
+          break;
+      }
+    }
+  };
+
+  return (
+    <S.Container>
+      <S.Title>로그인</S.Title>
+      <S.Form onSubmit={handleSubmit(handleLogin)}>
+        <S.InputContainer>
+          <S.InputBox>
+            <S.Label>이메일</S.Label>
+            <S.Input
+              {...register('email', {
+                required: '이메일을 입력하세요.',
+                pattern: {
+                  value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/,
+                  message: '유효한 이메일 주소를 입력해 주세요.',
+                },
+              })}
+              placeholder="이메일 주소를 입력하세요"
+            />
+            {errors.email && <S.ErrorText>{errors.email.message}</S.ErrorText>}
+          </S.InputBox>
+          <S.InputBox>
+            <S.Label>비밀번호</S.Label>
+            <S.Input
+              {...register('password', { required: '비밀번호를 입력하세요.' })}
+              type="password"
+              placeholder="비밀번호를 입력하세요"
+            />
+            {errors.password && (
+              <S.ErrorText>{errors.password.message}</S.ErrorText>
+            )}
+          </S.InputBox>
+        </S.InputContainer>
+        <S.FindAccountWrapper>
+          <S.FindAccountLink>이메일/비밀번호</S.FindAccountLink> 찾기
+        </S.FindAccountWrapper>
+        <Button type="submit">로그인</Button>
+        <S.SignUpWrapper>
+          아직 회원이 아니신가요?
+          <S.SignUpLink>회원가입</S.SignUpLink>
+        </S.SignUpWrapper>
+      </S.Form>
+    </S.Container>
+  );
+};
+
+export default Login;
