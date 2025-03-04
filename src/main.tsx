2816d412f39c160d6cfaafd5032216138730a0f7
임하림 <gkfla668@naver.com>
2025-03-04 15:05:52 +0900

    Refactor : 로그인 로직 커스텀 훅으로 분리 (#33)
    * package: 로그인을 위한 react-hook-form 설치
    
    * Feat : 로그인 페이지
    - 파이어베이스 로그인 기능 구현
    - 유효성 처리
    - 퍼블리싱
    
    * Style : 주석 추가
    
    * Design : 글로벌 스타일 수정
    
    * Feat : 회원가입 페이지 (기본)
    - 퍼블리싱
    - 회원가입 기능 구현
    - 유효성 처리
    
    * Feat : 이름/비밀번호 확인 input 추가, 이름 값 DB에 update
    
    * Rename : 폴더명 변경
    
    * Design : 필요없는 글로벌 스타일 제거
    
    * Design : 글로벌 스타일 초기화
    
    * Feat : App.css 비활성
    
    * Design : 그래픽 반응형 처리
    
    * Refactor : 필요없는 상태 값 제거
    
    * Refactor : 로그인 로직 custom hook으로 분리
    
    * Feat : 로그인 성공 후 '/' 으로 이동하는 로직 추가
    
    * Style : styled-components 명 수정
    
    * Merge branch 'dev' of https://github.com/Dev-FE-3/toy-project2-team3 into feature/login
    
    * Fix : firebase import 방식 수정

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,15 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import 'reset-css';
+
 import App from './App';
 import ThemeProvider from './providers/ThemeProvider';
+import GlobalStyle from './styles/createGlobalStyle';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <ThemeProvider>
+      <GlobalStyle />
       <App />
     </ThemeProvider>
   </StrictMode>
