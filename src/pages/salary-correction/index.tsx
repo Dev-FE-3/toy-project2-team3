610326eebc9a091808d7d05dc2ab3c461f4224d2
minsung <mingsung521@gmail.com>
2025-03-06 04:00:14 +0900

    Feature : 정정 신청 폼 퍼블리싱 및 기능 구현 완료

--- src/pages/salary-correction/index.tsx
+++ src/pages/salary-correction/index.tsx
@@ -1,25 +1,69 @@
 import { ReactNode, JSX, useRef, useEffect, useState } from 'react';
 import * as S from '../../pages/salary-correction/style';
 import { useNavigate } from 'react-router-dom';
-import Dropdown from '@/shared/dropdown/Dropdown';
+import Dropdown, { OptionType } from '@/shared/dropdown/Dropdown';
 import Button from '@/shared/button/Button';
 
 interface SalaryCorrectionPageProps {
   children?: ReactNode;
 }
 
+interface FormDataType {
+  salaryLabel: OptionType | null;
+  userId: string;
+  reason: string;
+  details: string;
+}
+
 const SalaryCorrectionPage = ({
   children,
 }: SalaryCorrectionPageProps): JSX.Element => {
   const navigate = useNavigate();
+  const [formData, setFormData] = useState<FormDataType>({
+    salaryLabel: null,
+    userId: '',
+    reason: '',
+    details: '',
+  });
 
-  const options = [
-    { label: '1', value: 1 },
-    { label: '2', value: 2 },
-    { label: '3', value: 3 },
-    { label: '4', value: 4 },
+  useEffect(() => {
+    console.log('Updated formData:', formData);
+  }, [formData]);
+
+  const dropdownOptions: OptionType[] = [
+    { label: '24년 9월 급여', value: 1 },
+    { label: '24년 11월 급여', value: 2 },
+    { label: '25년 1월 급여', value: 3 },
+    { label: '25년 4월 급여', value: 4 },
   ];
 
+  const handleSubmit = (event: React.FormEvent) => {
+    event.preventDefault();
+    const submitter = (event.nativeEvent as SubmitEvent)
+      .submitter as HTMLButtonElement;
+    if (submitter.name === 'submitBtn') {
+      setFormData(() => ({
+        salaryLabel: null,
+        userId: '',
+        reason: '',
+        details: '',
+      }));
+      console.log('제출하기 완료');
+    }
+  };
+
+  const handleSelect = (option: OptionType) => {
+    if (formData.salaryLabel !== option) {
+      setFormData((prev) => ({ ...prev, salaryLabel: option }));
+    }
+  };
+
+  const handleInputChange = (
+    event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    const { name, value } = event.target;
+    setFormData((prev) => ({ ...prev, [name]: value }));
+  };
   return (
     <>
       <S.BackLink onClick={() => navigate(-1)}>&lt; 이전 페이지로</S.BackLink>
@@ -28,14 +72,45 @@ const SalaryCorrectionPage = ({
         <S.ListTile>
           <S.TileTitle>신청 내역</S.TileTitle>
         </S.ListTile>
-        <S.FormTile>
+        <S.FormTile onSubmit={() => {}}>
           <S.TileTitle>급여 신청서 작성</S.TileTitle>
-          <Dropdown title="급여 일자를 선택해주세요" options={options} />
-          <S.InputForm onSubmit={() => {}}>
-            <S.Input placeholder="사번을 입력해주세요" />
-            <S.Input placeholder="정정 사유를 입력해주세요" />
-            <S.Input placeholder="상세 설명을 입력해주세요" />
-            <Button typeStyle="rounded" variant="filled">
+
+          <S.InputForm
+            onSubmit={(event) => {
+              handleSubmit(event);
+            }}
+          >
+            <Dropdown
+              title="급여 일자를 선택해주세요"
+              options={dropdownOptions}
+              width="100%"
+              onSelect={(option) => handleSelect(option)}
+            />
+            <S.Input
+              value={formData.userId}
+              name="userId"
+              onChange={(event) => handleInputChange(event)}
+              placeholder="사번을 입력해주세요"
+            />
+            <S.Input
+              value={formData.reason}
+              name="reason"
+              onChange={(event) => handleInputChange(event)}
+              placeholder="정정 사유를 입력해주세요"
+            />
+            <S.Textarea
+              value={formData.details}
+              name="details"
+              onChange={(event) => handleInputChange(event)}
+              placeholder="상세 설명을 입력해주세요"
+              rows={1} // 최소 1줄
+            />
+            <Button
+              name="submitBtn"
+              type="submit"
+              typeStyle="rounded"
+              variant="filled"
+            >
               제출하기
             </Button>
           </S.InputForm>
--- src/pages/salary-correction/style.tsx
+++ src/pages/salary-correction/style.tsx
@@ -50,13 +50,29 @@ export const FormTile = styled(Tile)`
 `;
 
 export const InputForm = styled.form`
+  height: 100%;
   display: flex;
   flex-direction: column;
+  align-items: flex-end;
   gap: 16px;
 `;
 
 export const Input = styled.input`
   width: 100%;
+  box-sizing: border-box;
+  border-radius: 8px;
+  border: 1px solid #b2b2b2;
+  padding: 11px 16px;
+  ${({ theme }) => theme.typography.body3}
+  &:focus {
+    outline: none;
+  }
+`;
+export const Textarea = styled.textarea`
+  width: 100%;
+  height: 100%;
+  overflow-y: hidden;
+  resize: none;
   box-sizing: border-box;
   border-radius: 8px;
   border: 1px solid #b2b2b2;
