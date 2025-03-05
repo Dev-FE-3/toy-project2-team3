250eba1da9b9bf865cfdd00311e6b2e0f24ec668
minsung <mingsung521@gmail.com>
2025-03-06 01:19:57 +0900

    Feat : 리스트 제외 스타일링 작업, 세부 수정 필요

--- src/pages/salary-correction/index.tsx
+++ src/pages/salary-correction/index.tsx
@@ -1,4 +1,8 @@
-import { ReactNode, JSX } from 'react';
+import { ReactNode, JSX, useRef, useEffect, useState } from 'react';
+import * as S from '../../pages/salary-correction/style';
+import { useNavigate } from 'react-router-dom';
+import Dropdown from '@/shared/dropdown/Dropdown';
+import Button from '@/shared/button/Button';
 
 interface SalaryCorrectionPageProps {
   children?: ReactNode;
@@ -7,7 +11,38 @@ interface SalaryCorrectionPageProps {
 const SalaryCorrectionPage = ({
   children,
 }: SalaryCorrectionPageProps): JSX.Element => {
-  return <>salarycorrectin</>;
+  const navigate = useNavigate();
+
+  const options = [
+    { label: '1', value: 1 },
+    { label: '2', value: 2 },
+    { label: '3', value: 3 },
+    { label: '4', value: 4 },
+  ];
+
+  return (
+    <>
+      <S.BackLink onClick={() => navigate(-1)}>&lt; 이전 페이지로</S.BackLink>
+      <S.Title>급여 정정 신청하기</S.Title>
+      <S.TileContainer>
+        <S.ListTile>
+          <S.TileTitle>신청 내역</S.TileTitle>
+        </S.ListTile>
+        <S.FormTile>
+          <S.TileTitle>급여 신청서 작성</S.TileTitle>
+          <Dropdown title="급여 일자를 선택해주세요" options={options} />
+          <S.InputForm onSubmit={() => {}}>
+            <S.Input placeholder="사번을 입력해주세요" />
+            <S.Input placeholder="정정 사유를 입력해주세요" />
+            <S.Input placeholder="상세 설명을 입력해주세요" />
+            <Button typeStyle="rounded" variant="filled">
+              제출하기
+            </Button>
+          </S.InputForm>
+        </S.FormTile>
+      </S.TileContainer>
+    </>
+  );
 };
 
 export default SalaryCorrectionPage;
---
+++ src/pages/salary-correction/style.tsx
@@ -0,0 +1,68 @@
+import styled from 'styled-components';
+
+export const BackLink = styled.a`
+  display: inline-block;
+  margin-top: 80px;
+  all: 'unset';
+  cursor: pointer;
+  ${({ theme }) => theme.typography.body2}
+  font-weight : bold;
+`;
+
+export const Title = styled.h1`
+  ${({ theme }) => theme.typography.heading2};
+  color: ${({ theme }) => theme.colors.black};
+`;
+
+export const TileTitle = styled.h1`
+  ${({ theme }) => theme.typography.heading3};
+  color: ${({ theme }) => theme.colors.black};
+`;
+
+export const TileContainer = styled.section`
+  width: 100%;
+  height: 572px;
+  display: flex;
+  gap: 20px;
+  margin-top: 12px;
+`;
+
+const Tile = styled.section`
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  border-radius: 16px;
+  padding: 28px 20px;
+`;
+
+export const ListTile = styled(Tile)`
+  flex-grow: 4;
+  min-width: 505px;
+  background-color: transparent;
+  border: 1.5px ${({ theme }) => theme.colors.point1} solid;
+`;
+
+export const FormTile = styled(Tile)`
+  display: flex;
+  gap: 28px;
+  flex-grow: 6;
+  background-color: ${({ theme }) => theme.colors.point2};
+`;
+
+export const InputForm = styled.form`
+  display: flex;
+  flex-direction: column;
+  gap: 16px;
+`;
+
+export const Input = styled.input`
+  width: 100%;
+  box-sizing: border-box;
+  border-radius: 8px;
+  border: 1px solid #b2b2b2;
+  padding: 11px 16px;
+  ${({ theme }) => theme.typography.body3}
+  &:focus {
+    outline: none;
+  }
+`;
