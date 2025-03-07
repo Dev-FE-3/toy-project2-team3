3cd9565ee719322349fba69b984d52d34be84ced
jiwonnayoung <lny1203@naver.com>
2025-03-07 17:50:49 +0900

    Chore: 사용하지 않는 코드 제거

--- src/firebase.tsx
+++ src/firebase.tsx
@@ -1,7 +1,6 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
-import { getStorage } from 'firebase/storage';
 
 interface ImportMetaEnv {
   VITE_FIREBASE_APP_KEY: string;
@@ -34,4 +33,3 @@ export const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
 export const db = getFirestore(app);
-export const storage = getStorage(app);
