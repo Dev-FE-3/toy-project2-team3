0ff15ea3833d51afbcd78dad2678997ce8dbf5d6
jiwonnayoung <lny1203@naver.com>
2025-03-18 10:38:31 +0900

    Refactor: formatDate에 dayjs 적용

--- src/utils/formatDate.ts
+++ src/utils/formatDate.ts
@@ -1,8 +1,6 @@
+import dayjs from 'dayjs';
+
 export const formatDate = (rawDate?: number) => {
   if (!rawDate) return '날짜 없음';
-  const date = new Date(rawDate);
-  const year = date.getFullYear();
-  const month = String(date.getMonth() + 1).padStart(2, '0');
-  const day = String(date.getDate()).padStart(2, '0');
-  return `${year}년 ${month}월 ${day}일`;
+  return dayjs(rawDate).format('YYYY년 MM월 DD일');
 };
